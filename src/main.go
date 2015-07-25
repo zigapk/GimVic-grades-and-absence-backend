eca3d58986436390b5df78b775ef9e73907c4d0c
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-07-25 14:55:20 +0200

    dummy choosing functionality added

--- src/main.go
+++ src/main.go
@@ -4,53 +4,50 @@ import (
     "fmt"
     "net/http"
     "database/sql"
+    "net/url"
     _ "github.com/go-sql-driver/mysql"
+    "encoding/json"
 )
 
 var sqlString string = "root:root@/soc"
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "numberOf ucencev: ", numberOf("gender", ""), "\n")
-	fmt.Fprint(w, "numberOf moskih: ", numberOf("gender", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "numberOf zensk: ", numberOf("gender", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecni uspeh: ", average("final", ""), "\n")
-	fmt.Fprint(w, "Povprecni uspeh moskih: ", average("final", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecni uspeh zensk: ", average("final", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "average ocena: ", average("average", ""), "\n")
-	fmt.Fprint(w, "average ocena moskih: ", average("average", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "average ocena zensk: ", average("average", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur: ", average("opravicene", ""), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur pri moskih: ", average("opravicene", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur pri zenskah: ", average("opravicene", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 1. letniku: ", average("opravicene", "class = 1"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 2. letniku: ", average("opravicene", "class = 2"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 3. letniku: ", average("opravicene", "class = 3"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 4. letniku: ", average("opravicene", "class = 4"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur: ", average("neopravicene", ""), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur pri moskih: ", average("neopravicene", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur pri zenskah: ", average("neopravicene", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 1. letniku: ", average("neopravicene", "class = '1'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 2. letniku: ", average("neopravicene", "class = '2'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 3. letniku: ", average("neopravicene", "class = '3'"), "\n")
-	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 4. letniku: ", average("neopravicene", "class = '4'"), "\n")
-	fmt.Fprint(w, "numberOf dijakov z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0"), "\n")
-	fmt.Fprint(w, "numberOf moskih z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0 AND gender='M'"), "\n")
-	fmt.Fprint(w, "numberOf zensk z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0 AND gender='Z'"), "\n")
-	fmt.Fprint(w, "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (5): ", average("opravicene", "final = 5"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (4): ", average("opravicene", "final = 4"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (3): ", average("opravicene", "final = 3"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (2): ", average("opravicene", "final = 2"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (1): ", average("opravicene", "final = 1"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (0): ", average("opravicene", "final = 0"), "\n")
-	fmt.Fprint(w, "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (5): ", average("neopravicene", "final = 5"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (4): ", average("neopravicene", "final = 4"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (3): ", average("neopravicene", "final = 3"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (2): ", average("neopravicene", "final = 2"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (1): ", average("neopravicene", "final = 1"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (0): ", average("neopravicene", "final = 0"), "\n")
+    queries := parseUrl(r)
+    
+    var where string = ""
+    var i int = 0
+    for name, values := range queries {
+    	if i != 0 {
+    		where += " and "
+    	}
+    	where += name + "=" + values[0]
+    	i++
+    }
+    var optionalAnd string = ""
+    if where != "" {optionalAnd = " and "}
+
+    response := &Response {
+    	Facts: Facts {
+    		AllStudents: numberOf("gender", ""),
+    		CurrentStudents: numberOf("gender", where),
+    		AllMale: numberOf("gender", "gender = 'M'"),
+    		CurrentMale: numberOf("gender", where + optionalAnd + "gender = 'M'"),
+    		AllFemale: numberOf("gender", "gender = 'Z'"),
+    		CurrentFemale: numberOf("gender", where + optionalAnd + "gender = 'Z'"),
+    	},
+    }
+    responseStr, err := json.Marshal(response)
+    check(err)
+    fmt.Fprint(w, string(responseStr))
 }
+ func parseUrl(r *http.Request) map[string][]string{
+ 	str := r.URL.String()
+    u, err := url.Parse(str)
+    check(err)
+    m, err := url.ParseQuery(u.RawQuery)
+    check(err)
+    return m
+ }
 
 func main() {
     http.HandleFunc("/", handler)
@@ -64,6 +61,9 @@ func numberOf(what string, where string) int {
 	} else {
 		query = "select " + what + " from soc where " + where + ";"
 	}
+
+	//debug
+	fmt.Println("EXECUTING: " + query)
 
 	con, err := sql.Open("mysql", sqlString)
 	check(err)
@@ -115,4 +115,29 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+
+type Response struct {
+    Facts Facts
+    Stats Stats
+}
+
+type Facts struct {
+	AllStudents int
+	CurrentStudents int
+	AllMale int
+	CurrentMale int
+	AllFemale int
+	CurrentFemale int
+
+}
+
+type Stats struct {
+	AverageGrade float64
+	AverageAbsence float64
+	ExcusableAbsence float64
+	InexcusableAbsence float64
+	ExcusableStudents int
+	InexcusableStudents int
 }
