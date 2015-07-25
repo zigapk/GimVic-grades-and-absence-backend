014e66bfc475927072ca9d170423eb2992a1a2f7
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-07-25 12:23:11 +0200

    translated method names

--- src/main.go
+++ src/main.go
@@ -10,46 +10,46 @@ import (
 var sqlString string = "root:root@/soc"
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Stevilo ucencev: ", stevilo("gender", ""), "\n")
-	fmt.Fprint(w, "Stevilo moskih: ", stevilo("gender", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Stevilo zensk: ", stevilo("gender", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecni uspeh: ", povprecna("final", ""), "\n")
-	fmt.Fprint(w, "Povprecni uspeh moskih: ", povprecna("final", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecni uspeh zensk: ", povprecna("final", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecna ocena: ", povprecna("average", ""), "\n")
-	fmt.Fprint(w, "Povprecna ocena moskih: ", povprecna("average", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecna ocena zensk: ", povprecna("average", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur: ", povprecna("opravicene", ""), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur pri moskih: ", povprecna("opravicene", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur pri zenskah: ", povprecna("opravicene", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur v 1. letniku: ", povprecna("opravicene", "class = 1"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur v 2. letniku: ", povprecna("opravicene", "class = 2"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur v 3. letniku: ", povprecna("opravicene", "class = 3"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo opravicenih ur v 4. letniku: ", povprecna("opravicene", "class = 4"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur: ", povprecna("neopravicene", ""), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur pri moskih: ", povprecna("neopravicene", "gender = 'M'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur pri zenskah: ", povprecna("neopravicene", "gender = 'Z'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur v 1. letniku: ", povprecna("neopravicene", "class = '1'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur v 2. letniku: ", povprecna("neopravicene", "class = '2'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur v 3. letniku: ", povprecna("neopravicene", "class = '3'"), "\n")
-	fmt.Fprint(w, "Povprecno stevilo neopravicenih ur v 4. letniku: ", povprecna("neopravicene", "class = '4'"), "\n")
-	fmt.Fprint(w, "Stevilo dijakov z neopravicenimi urami: ", stevilo("neopravicene", "neopravicene != 0"), "\n")
-	fmt.Fprint(w, "Stevilo moskih z neopravicenimi urami: ", stevilo("neopravicene", "neopravicene != 0 AND gender='M'"), "\n")
-	fmt.Fprint(w, "Stevilo zensk z neopravicenimi urami: ", stevilo("neopravicene", "neopravicene != 0 AND gender='Z'"), "\n")
+    fmt.Fprint(w, "numberOf ucencev: ", numberOf("gender", ""), "\n")
+	fmt.Fprint(w, "numberOf moskih: ", numberOf("gender", "gender = 'M'"), "\n")
+	fmt.Fprint(w, "numberOf zensk: ", numberOf("gender", "gender = 'Z'"), "\n")
+	fmt.Fprint(w, "Povprecni uspeh: ", average("final", ""), "\n")
+	fmt.Fprint(w, "Povprecni uspeh moskih: ", average("final", "gender = 'M'"), "\n")
+	fmt.Fprint(w, "Povprecni uspeh zensk: ", average("final", "gender = 'Z'"), "\n")
+	fmt.Fprint(w, "average ocena: ", average("average", ""), "\n")
+	fmt.Fprint(w, "average ocena moskih: ", average("average", "gender = 'M'"), "\n")
+	fmt.Fprint(w, "average ocena zensk: ", average("average", "gender = 'Z'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur: ", average("opravicene", ""), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur pri moskih: ", average("opravicene", "gender = 'M'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur pri zenskah: ", average("opravicene", "gender = 'Z'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 1. letniku: ", average("opravicene", "class = 1"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 2. letniku: ", average("opravicene", "class = 2"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 3. letniku: ", average("opravicene", "class = 3"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf opravicenih ur v 4. letniku: ", average("opravicene", "class = 4"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur: ", average("neopravicene", ""), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur pri moskih: ", average("neopravicene", "gender = 'M'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur pri zenskah: ", average("neopravicene", "gender = 'Z'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 1. letniku: ", average("neopravicene", "class = '1'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 2. letniku: ", average("neopravicene", "class = '2'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 3. letniku: ", average("neopravicene", "class = '3'"), "\n")
+	fmt.Fprint(w, "Povprecno numberOf neopravicenih ur v 4. letniku: ", average("neopravicene", "class = '4'"), "\n")
+	fmt.Fprint(w, "numberOf dijakov z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0"), "\n")
+	fmt.Fprint(w, "numberOf moskih z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0 AND gender='M'"), "\n")
+	fmt.Fprint(w, "numberOf zensk z neopravicenimi urami: ", numberOf("neopravicene", "neopravicene != 0 AND gender='Z'"), "\n")
 	fmt.Fprint(w, "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (5): ", povprecna("opravicene", "final = 5"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (4): ", povprecna("opravicene", "final = 4"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (3): ", povprecna("opravicene", "final = 3"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (2): ", povprecna("opravicene", "final = 2"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (1): ", povprecna("opravicene", "final = 1"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (0): ", povprecna("opravicene", "final = 0"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (5): ", average("opravicene", "final = 5"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (4): ", average("opravicene", "final = 4"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (3): ", average("opravicene", "final = 3"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (2): ", average("opravicene", "final = 2"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (1): ", average("opravicene", "final = 1"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo opravicenih ur dijakov (0): ", average("opravicene", "final = 0"), "\n")
 	fmt.Fprint(w, "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (5): ", povprecna("neopravicene", "final = 5"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (4): ", povprecna("neopravicene", "final = 4"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (3): ", povprecna("neopravicene", "final = 3"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (2): ", povprecna("neopravicene", "final = 2"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (1): ", povprecna("neopravicene", "final = 1"), "\n")
-	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (0): ", povprecna("neopravicene", "final = 0"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (5): ", average("neopravicene", "final = 5"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (4): ", average("neopravicene", "final = 4"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (3): ", average("neopravicene", "final = 3"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (2): ", average("neopravicene", "final = 2"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (1): ", average("neopravicene", "final = 1"), "\n")
+	fmt.Fprint(w, "Povprecno stevlilo neopravicenih ur dijakov (0): ", average("neopravicene", "final = 0"), "\n")
 }
 
 func main() {
@@ -57,7 +57,7 @@ func main() {
     http.ListenAndServe(":8080", nil)
 }
 
-func stevilo(what string, where string) int {
+func numberOf(what string, where string) int {
 	var query string
 	if where == "" {
 		query = "select " + what + " from soc;"
@@ -79,7 +79,7 @@ func stevilo(what string, where string) int {
 	return i
 }
 
-func povprecna(what, where string) float64 {
+func average(what, where string) float64 {
 	var query string
 	if where == "" {
 		query = "select " + what + " from soc;"
